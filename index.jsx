d3b148274c740eb22876f1e479a0df26fda595ac
PARTHSH1 <110349699+PARTHSH1@users.noreply.github.com>
2024-09-16 16:33:46 +0530

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>index</div>
+  )
+}
+=======
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>index</div>
+  )
+}
+>>>>>>> c992be79 (Initial commit for gh-pages branch)
